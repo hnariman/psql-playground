c13c5c0553c49ec7a4722251a545b3c0f6317d99
Nariman Huseynov <hnariman@gmail.com>
2023-02-09 01:54:54 +0400

    uppercased :)

--- init.sql
+++ init.sql
@@ -1,63 +1,63 @@
-create table users (
+CREATE TABLE users (
   id serial primary key,
   first varchar(50) not null,
   last varchar(50) not null,
   email varchar(50) not null
 );
 
-insert into users (first, last, email) values 
-('James', 'Brown', 'james.brown@mail.com'),
-('Buddy', 'Guy', 'buddy.guy@mail.com'),
-('Eric', 'Clapton', 'eric.clapton@mail.com'),
-('Billie', 'Gibbons', 'billie.gibbons@mail.com'),
-('Liz', 'Fitzgerald', 'liz.fitzgerald@mail.com'),
-('Gary', 'Moore', 'garrym@mail.com'),
-('Phill', 'Collins', 'pcollins@mail.com'),
-('Dave', 'Brubeck', 'quartet@mail.com'),
-('Vanessa', 'Ma', 'mavanessa@mail.com'),
-('Clare','Duffy','clareduffy1505@outlook.com'),
-('Byron','Ortiz','byronortiz@yahoo.com'),
-('Marny','Hopper','marnyhopper@aol.com'),
-('Uriah','Wiggins','uriahwiggins@aol.com'),
-('Kai','Sloan','kaisloan3555@google.com');
+INSERT INTO users (first, last, email) VALUES 
+  ('James', 'Brown', 'james.brown@mail.com'),
+  ('Buddy', 'Guy', 'buddy.guy@mail.com'),
+  ('Eric', 'Clapton', 'eric.clapton@mail.com'),
+  ('Billie', 'Gibbons', 'billie.gibbons@mail.com'),
+  ('Liz', 'Fitzgerald', 'liz.fitzgerald@mail.com'),
+  ('Gary', 'Moore', 'garrym@mail.com'),
+  ('Phill', 'Collins', 'pcollins@mail.com'),
+  ('Dave', 'Brubeck', 'quartet@mail.com'),
+  ('Vanessa', 'Ma', 'mavanessa@mail.com'),
+  ('Clare','Duffy','clareduffy1505@outlook.com'),
+  ('Byron','Ortiz','byronortiz@yahoo.com'),
+  ('Marny','Hopper','marnyhopper@aol.com'),
+  ('Uriah','Wiggins','uriahwiggins@aol.com'),
+  ('Kai','Sloan','kaisloan3555@google.com');
 
 
-create type units as enum ('kg', 'gr', 'liter', 'ml');
+CREATE TYPE units AS ENUM ('kg', 'gr', 'liter', 'ml');
 
-create table ingredients (
+CREATE TABLE ingredients (
   id serial primary key,
   title varchar(100),
   description text,
   unit units
 );
 
-create table recipes (
+CREATE TABLE recipes (
   id serial primary key,
   title varchar(50),
   description varchar(50)
 );
 
-create table recipe_ingredients (
+CREATE TABLE recipe_ingredients (
 	id serial primary key,
 	recipe_id integer references recipes(id),
 	ingredient_id integer references ingredients(id),
 	amount int not null
 );
 
-insert into ingredients (title, description, unit) values
+INSERT INTO ingredients (title, description, unit) VALUES
 ('potato','bulba','kg'),
 ('caviar','black expensive egg','gr'),
 ('oil','petroleum','ml'),
 ('beer','wish unit tons was available','liter'),
 ('sugar','sweet life','gr');
 
-insert into recipes (title, description) values
+INSERT INTO recipes (title, description) VALUES
 ('cake','smells like heaven'),
 ('expensive','caviar cake'),
 ('clean drink','more beer'),
 ('potato','smells like chicken');
 
-insert into recipe_ingredients (recipe_id, ingredient_id, amount) values
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id, amount) VALUES
 (1, 5, 100),
 (1, 1, 1),
 (4, 1, 10),
